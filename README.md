e3cf4bb167aa814786f1e0aa0507434b4cb4cfba
codecollage <codecollage90@gmail.com>
2021-09-28 22:15:45 +0700

    Scripts, CodeBook.md and README.md to download, clean up and summarize Human Activity Recognition Using Smartphones Data Set.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Getting-and-cleaning-data
-Assignment for Getting and Cleaning Data Course
+Project assignment for Getting and Cleaning Data Course week 4
+Purpose: prepare tidy data that can be used for later analysis.
+
+Data Source
+===========
+Human Activity Recognition Using Smartphones Dataset downloaded from <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>. 
+Full description: <http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones>.
+
+
+Files:
+======
+- CodeBook.md: describes the variables, the data, and any transformations or work performed to clean up the data.
+
+- download_extract.R : an R script to download dataset
+
+- run_analysis.R: an R script to clean up the dataset and summarize data with output files saved to "output" folder.
+
+
+Outputs:
+========
+1. "x.csv" is the merged and tidied dataset from train and test dataset. Read CodeBook.md  that describes the variables, the data, and any transformations or work performed to clean up the data.
+2. "xmean.csv" is a summary of the average of each variable accross activities group.
+
+
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+# Import dplyr
+library(dplyr)
+
+#Set dataset path and read data
+datapath <- ".//UCI HAR Dataset"
+xtest <- read.table(paste(datapath, "test", "X_test.txt", sep = "\\"))
+ytest <- read.table(paste(datapath, "test", "y_test.txt", sep = "\\"))
+xtrain <- read.table(paste(datapath, "train", "X_train.txt", sep = "\\"))
+ytrain <- read.table(paste(datapath, "train", "y_train.txt", sep = "\\"))
+
+# 1. Merge dataset
+x <- rbind(xtrain, xtest)
+y <- rbind(ytrain, ytest)
+
+# Remove unused data
+rm(xtest, ytest, xtrain, ytrain)
+
+
+# 2. Extracts mean and SD measurement
+
+# Get features name data
+features <- read.table(paste(datapath, "features.txt", sep = "\\"))
+
+# Replace variable names with descriptive features name
+names(x) <- features$V2
+
+# Remove unused data
+rm(features)
+    
+# Extract variables
+x <- select(x, contains(c("mean()", "std")))
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+#read activity labels data
+activitylabels <- read.table(paste(datapath, "activity_labels.txt", sep = "\\"))
+
+#map activity numeric label to the descriptive activity names
+x$activity <- activitylabels[y$V1, 2]
+
+    # Remove unused data
+rm(activitylabels, y)
+
+# 4. Appropriately labels the data set with descriptive variable names
+
+# all lowercase
+names(x) <- tolower(names(x))
+
+# descriptive
+names(x) <- sub("tbody", "timebody", names(x))
+names(x) <- sub("tgravity", "timegravity", names(x))
+names(x) <- sub("fbody", "freqbody", names(x))
+names(x) <- sub("fgravity", "freqgravity", names(x))
+names(x) <- gsub("acc", "acceleration", names(x))
+names(x) <- gsub("mag", "magnitude", names(x))
+
+# contain no underscores/dot/space
+names(x) <- gsub("-", "", names(x))
+names(x) <- gsub("\\()", "", names(x))
+
+# 5. Independent tidy data set with the average of each variable 
+
+#    for each activity and each subject
+xmean <- aggregate(x[, !names(x) %in% "activity"], list(x$activity), mean)
+
+# Write tidy data and mean summary to the csv files
+dir.create(file.path(".\\output"), showWarnings = FALSE)
+write.csv(x, ".\\output\\x.csv")
+write.csv(xmean, ".\\output\\xmean.csv")
